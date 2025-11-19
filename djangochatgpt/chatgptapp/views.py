a4d9253769152ced63af6913f126acfe7c7d609b
0xvantrex <callagansikuche0@gmail.com>
2025-11-19 09:39:43 +0300

    refactor the code

--- djangochatgpt/chatgptapp/views.py
+++ djangochatgpt/chatgptapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'aboutus.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+def products(request):
+    return render(request, 'products.html')
+
+def services(request):
+    return render(request, 'services.html')
